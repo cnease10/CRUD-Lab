bb60e88c1decab52673092e3137c7d33cc1ce19b
cnease10 <cierra.nease10@gmail.com>
2019-10-16 14:13:00 -0600

    created dogs array

---
+++ models/dogs.js
@@ -0,0 +1,19 @@
+const dogs = [
+    {
+        name: alfredo,
+        age: 7,
+        breed: lab
+    },
+    {
+        name: moose,
+        age: 3,
+        breed: poodle
+    },
+    {
+        name: george,
+        age: 27,
+        breed: chihuahua
+    }
+]
+
+module.export = dogs;
