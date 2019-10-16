0d6c29eb1c1a9376fd71ba95f8e8e14b22feb399
cnease10 <cierra.nease10@gmail.com>
2019-10-16 15:23:28 -0600

    added a delete route

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 const Dogs = require('./models/dogs.js');
 
+
+//middle-ware
+app.use(methodOverride('_method'));
+
+//routes
 app.get('/dogs', (req, res) => {
     res.render('index.ejs', {
         dogs: Dogs
     })
 });
+
+app.delete('/dogs/:index', (req, res) => {
+    //delete logic
+    Dogs.splice(req.params.index, 1);
+    //redirect to homepage
+    res.redirect('/dogs');
+})
 
 
 app.listen(3000, () => {
